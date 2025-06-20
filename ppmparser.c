4dcdc225be6bce23e2d61635b6e38c2c94ac4ba9
iaroslav <varfolomeeviaroslav@gmail.com>
2025-06-20 18:52:14 +0100

    add parser for .pp files

---
+++ ppmparser.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define HEIGHT 128
+#define WIDTH 128
+#define SCALE 2
+#define SEP 0x0A
+
+int texture[HEIGHT * WIDTH][3] = {0};
+
+enum colors { red, green, blue };
+
+void parse(FILE* fptr) {
+    int c = 0;
+    int symbol;
+    for (symbol=0;symbol<3;) {
+        if ((c = fgetc(fptr)) == SEP)
+            symbol++;
+    }
+    int colorn, color_part;
+    for (colorn=0;colorn<HEIGHT * WIDTH;colorn++) {
+        for (color_part=red;color_part<=blue;color_part++) {
+            c = fgetc(fptr);
+            texture[colorn][color_part] = c / 255;
+        }
+    }
+}
